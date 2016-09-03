592152b9d33f8651a1d7ea506fa81098136a98ef
CheckYourScreen <fictitiousguy@yahoo.com>
2016-09-03 07:48:21 +0200

    DU-fy
    Signed-off-by: CheckYourScreen <fictitiousguy@yahoo.com>

--- du.mk
+++ du.mk
@@ -18,13 +18,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from onyx device
 $(call inherit-product, device/oneplus/onyx/device.mk)
 
-# Inherit some common GZR stuff.
-$(call inherit-product, vendor/broken/config/common_full_phone.mk)
+# Inherit some common  stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Call the proprietary setup
 $(call inherit-product-if-exists, vendor/oneplus/onyx/onyx-vendor.mk)
 
-PRODUCT_NAME := broken_onyx
+PRODUCT_NAME := du_onyx
 PRODUCT_DEVICE := onyx
 PRODUCT_MANUFACTURER := OnePlus
 
@@ -38,7 +38,5 @@ TARGET_VENDOR_PRODUCT_NAME := onyx
 PRODUCT_PACKAGES += \
     FMRadio
 	
-PRODUCT_BUILD_PROP_OVERRIDES += \
- 	DEVICE_MAINTAINERS="Nimit Mehta (CheckYourScreen)"
 
 	
